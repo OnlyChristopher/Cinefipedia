2421e25d9e7b03ebd7cd030dbe2086925972343b
kankkuna <karina842@hotmail.com>
2018-01-17 06:48:24 -0500

    avance vista principal y modal

--- js/app.js
+++ js/app.js
@@ -9,3 +9,4 @@ $(document).ready(function(){
       }, 5000);
 
 });
+class="btn btn-primary video-btn" data-toggle="modal" data-src="https://www.youtube.com/embed/Jfrjeg26Cwk" data-target="#myModal"
