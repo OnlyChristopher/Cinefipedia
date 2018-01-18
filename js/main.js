d7ef1f6a93c76474536babb2e609a667e17c1e0b
Danielalab <olabolacrayola@hotmail.com>
2018-01-18 16:50:23 -0500

    funcionalidad para modal de peliculas

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // script sidebar toggled
-$('#menu-toggle').click(function(event) {
+$('#menu-toggle').click(function (event) {
   event.preventDefault();
   $('#wrapper').toggleClass('toggled');
 });
@@ -14,7 +14,13 @@ $(document).ready(() => {
   var itemComedy = $('.comedy');
   var itemHorror = $('.horror');
   var itemRomance = $('.romance');
-
+  var titleModal = $('.title-movie');
+  var synopsisModal = $('#synopsis');
+  var actorsModal = $('#actors');
+  var releaseDatesModal = $('#release-dates');
+  var voteAverageModal = $('#vote-average');
+  var imgModal = $('#img-modal');
+  console.log(titleModal);
   // asociando eventos a elementos del DOM
   inputSearch.on('submit', (event) => {
     event.preventDefault();
@@ -30,7 +36,7 @@ $(document).ready(() => {
   itemHorror.on('click', searchDataGenre);
   itemRomance.on('click', searchDataGenre);
 
-  // obtner peliculas segun el genero seleccionado
+  // obtener peliculas segun el genero seleccionado
   function searchDataGenre() {
     var codeGenre = $(this).attr('data-code');
     var nameSectionTab = '.' + $(this).attr('aria-controls');
@@ -45,29 +51,57 @@ $(document).ready(() => {
           moviesHtml += `
           <div class="col-6 col-sm-4 col-md-2">
             <div class="text-center">
-              <img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}" class="img-fluid">
+              <img src="http://image.tmdb.org/t/p/w185/${movie.poster_path}" class="img-fluid selected-movie" data-id="${movie.id}" data-toggle="modal" data-target=".bd-example-modal-lg">
               <h5>${movie.title}</h5>
-              <a class="selected-movie" data-id="${movie.id}" href="#">Movie Details</a>
+              <a data-id="${movie.id}" href="#">Movie Details</a>
             </div>
           </div>
         `;
         });
         $(nameSectionTab).html(moviesHtml);
-        console.log($('.selected-movie'));
-        $('.selected-movie').click(function(event) {
+        $('.selected-movie').click(function (event) {
+          console.log($('.selected-movie'));
           event.preventDefault();
           console.log('hice click');
           var id = $(this).attr('data-id');
           // sessionStorage.setItem('movieId', id);
-          var dataMovie = 'https://api.themoviedb.org/3/movie/' + id + '?api_key=5076f0f992d07860e10ee70c4f034e5e';
-          console.log(dataMovie);
+          getMovieData(id);
+
           // return false;
         });
       }).catch((err) => {
         console.log(err);
       });
   };
+  // funcion para mostrar la informacion de la pelicula seleccionada
+  function getMovieData(id) {
+    var dataMovie = 'https://api.themoviedb.org/3/movie/' + id + '?api_key=5076f0f992d07860e10ee70c4f034e5e';
+    var creditMovieData = 'https://api.themoviedb.org/3/movie/' + id + '/credits?api_key=5076f0f992d07860e10ee70c4f034e5e';
+    var listCastMovie = [];
+    $.getJSON(creditMovieData)
+      .then((result) => {
+        console.log(result);
+        for (let index = 0; index < 11; index++) {
+          const nameCast = result['cast'][index]['name'];
+          listCastMovie.push(nameCast);
+        }
+      });
+    $.getJSON(dataMovie)
+      .then((result) => {
+        console.log(result);
+        listCastMovie = listCastMovie.toString();
+        console.log(listCastMovie);
+        titleModal.text(result.title);
+        imgModal.attr('src', 'http://image.tmdb.org/t/p/w185/' + result.poster_path);
+        synopsisModal.text(result.overview);
+        actorsModal.text(listCastMovie);
+        voteAverageModal.text(result.vote_average);
+        releaseDatesModal.text(result.release_date);
+      });
+  }
 });
+
+
 
 // traer todas las peliculas relacionadas a lo que el usuario escribio en el input
 function getMovies(searchText) {
@@ -90,7 +124,7 @@ function getMovies(searchText) {
 
       $('#movies').html(moviesHtml);
       console.log($('.selected-movie'));
-      $('.selected-movie').click(function(event) {
+      $('.selected-movie').click(function (event) {
         event.preventDefault();
         console.log('hice click');
         var id = $(this).attr('data-id');
@@ -104,7 +138,7 @@ function getMovies(searchText) {
     });
 }
 
-function getMovie() {
+/*function getMovie() {
   let movieId = sessionStorage.getItem('movieId');
 
   $.getJSON('http://www.omdbapi.com?i=' + movieId + '&apikey=bea6c355')
@@ -146,4 +180,4 @@ function getMovie() {
     .catch((err) => {
       console.log(err);
     });
-}
+}*/
